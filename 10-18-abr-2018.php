b44dd1eafababb3ff79faf61c26bc591fc532cf4
Comunidad de PHP <comunidaddephp@gmail.com>
2018-04-18 21:44:41 -0500

    Create 10-18-abr-2018.php

---
+++ 10-18-abr-2018.php
@@ -0,0 +1,48 @@
+<?php
+//Autor: Claudio Morales
+//Fecha: 18 abril 2018
+//Video del Webinar:
+
+class OperacionesAritmeticas
+  {
+     public function estructura()
+     {
+       $valoruno = "Bol";
+       $valordos = "Arg";
+       $edad = 18;
+       $resultado = ($edad>=18) ? "SI entra" : "No entra";
+       return $resultado;
+     }
+     public function operaciones($datosR, $operacion="divide")
+      {
+       $resultado='';
+       switch($operacion)
+       {
+         case "suma":
+           $resultado = $datosR[0] + $datosR[1] + $datosR[2]+ $datosR[3]+$datosR[4];
+           break;
+         case "resta":
+           $resultado = $datosR[0] - $datosR[1] - $datosR[2] - $datosR[3];
+           break;
+         case "multiplica":
+            $resultado = $datosR[0] * $datosR[1] * $datosR[2] * $datosR[3];
+            break;
+         case "divide":
+           $resultado = $datosR[0] / $datosR[1] / $datosR[2] / $datosR[3];
+           break;
+       }
+        return $resultado;
+      }    
+  }
+$datos = array(1,2,3,4,5,6,7);
+$objeto = new  OperacionesAritmeticas();
+//$objeto->arreglo();
+echo "valor es: ".$objeto->operaciones($datos,"suma");  
+//echo "Valor es: ".$objeto->estructura();
+
+//Tarea
+
+
+
+
+
