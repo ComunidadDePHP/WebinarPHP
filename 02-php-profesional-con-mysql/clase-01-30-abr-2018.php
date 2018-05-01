327643f58373e2495830b783cb078e6aea2de606
Comunidad de PHP <comunidaddephp@gmail.com>
2018-04-30 22:01:57 -0500

    código de clase
    Conectar via objeto de mysqli

--- 02-php-profesional-con-mysql/clase-01-30-abr-2018.php
+++ 02-php-profesional-con-mysql/clase-01-30-abr-2018.php
@@ -2,3 +2,18 @@
 //Autor: Claudio Morales
 //Fecha: 30 abril 2018
 //Video del Webinar: https://www.facebook.com/ComunidadDePeHPe/videos/1516776408432484/
+
+$server = "localhost";
+$usuario = "root";
+$pass = "";
+$nombreBase = "nuevabase";
+
+//La siguiente forma de conectar no la usaremos
+//$conexion = mysqli_connect();
+//La anterior forma de conectar no la usaremos
+
+$conexion = new mysqli($server,$usuario,$pass, $nombreBase);
+echo "tipo variable: ".gettype($conexion)."<br>";
+echo "contenido variable<br>";
+var_dump($conexion);
+echo "<br><br>";
