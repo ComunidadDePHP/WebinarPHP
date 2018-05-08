d38ad6dca5e02790897ebf534e8f5cbf9db0d9fb
Comunidad de PHP <comunidaddephp@gmail.com>
2018-05-07 22:13:20 -0500

    Create clase-04-07-may-2018-index.php

---
+++ 02-php-profesional-con-mysql/clase-04-07-may-2018-index.php
@@ -0,0 +1,20 @@
+<?php
+//Autor: Claudio Morales
+//Fecha: 07 mayo 2018
+//Video del Webinar: https://www.facebook.com/ComunidadDePeHPe/videos/1524145311028927/
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Mi Sistema de Publicaciones</title>
+  </head>
+  <body>
+    <div style="text-align:center;" >
+      Mi sistema de publicacio es
+    </div>
+    <div style="text-align:center;">
+      Inicio | Nuevo Editor | Nueva Noticia | Editar Noticia |
+    </div>
+
+  </body>
+</html>
