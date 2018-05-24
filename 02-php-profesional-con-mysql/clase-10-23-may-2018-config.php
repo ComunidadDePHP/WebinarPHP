afb366eb598b927086576556bbfbe719da11239c
Comunidad de PHP <comunidaddephp@gmail.com>
2018-05-23 21:54:21 -0500

    Create clase-10-23-may-2018-config.php

---
+++ 02-php-profesional-con-mysql/clase-10-23-may-2018-config.php
@@ -0,0 +1,11 @@
+<?php
+//Autor: Claudio Morales
+//Fecha: 23 mayo 2018
+//Video del Webinar: 
+
+define("SERVER",      "localhost");
+define("USUARIO",     "usermay2018");
+define("PASS",        "Sistema_1234");
+define("NOMBREBASE",  "usermay2018");
+//define("TIPOCONEXION",  "mysqli");
+define("TIPOCONEXION",  "pdomysql");
